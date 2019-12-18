02a963e53850e4488d88471073bfcba5234743ba
Peter Holló <lordpejdar@gmail.com>
2019-12-18 10:27:27 +0000

    Fix: Unable to run styleguidist in Gitpod
    https://github.com/styleguidist/react-styleguidist/issues/669#issuecomment-342177583

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+    webpackConfig.devServer = {
+      disableHostCheck: true,
+    };
+    return webpackConfig;
+  },
   styleguideDir: "docs"
 };
