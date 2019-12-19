aedbe88cdee6718e1d3f6f76114716bd9a3cd3b8
Peter Holló <lordpejdar@gmail.com>
2019-12-19 16:04:29 +0000

    Use tailwind in current components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import AppHeader from "./components/AppHeader";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto m-4 p-2 border rounded-lg">
       <AppHeader logo={logo} link="https://reactjs.org" />
     </div>
   );
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,19 +4,23 @@ import PropTypes from "prop-types";
 function AppHeader(props) {
   const { logo, link } = props;
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
+    <header className="md:flex items-center justify-center">
+      <div className="md:flex-shrink-0">
+        <img src={logo} className="w-56" alt="logo" />
+      </div>
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      <a
-        className="App-link"
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
+        <a
+          className="App-link"
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
+      </div>
     </header>
   )
 }
