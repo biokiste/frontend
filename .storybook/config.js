52e02349da30490a4bfe90c049c24dbdcc732a5a
Peter Holló <lordpejdar@gmail.com>
2019-12-28 15:53:05 +0100

    Use storybook with some addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
 
 import "../src/styles/tailwind.css";
 
 configure(require.context("../src", true, /\.stories\.js$/), module);
+addDecorator(withA11y);
+addParameters({ viewport: { viewports: newViewports } });
