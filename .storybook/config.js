f269b63380de8bc6eacf10007bce7cfd9329fc27
Peter Holló <lordpejdar@gmail.com>
2020-01-07 20:47:31 +0100

    Fix: "newViewports" not defined

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 import "../src/styles/tailwind.css";
 
 configure(require.context("../src", true, /\.stories\.js$/), module);
 addDecorator(withA11y);
-addParameters({ viewport: { viewports: newViewports } });
+addParameters({ viewport: { viewports: INITIAL_VIEWPORTS } });
