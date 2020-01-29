12da31bed9ec9977ef766046da7221951171b647
Peter Holló <lordpejdar@gmail.com>
2020-01-29 11:28:21 +0100

    Fix: i18n not working with storybook

--- src/stories/Transaction.stories.js
+++ src/stories/Transaction.stories.js
@@ -2,6 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import TransactionList from "../components/transaction/TransactionList";
 import { TransactionProvider } from "../components/transaction/TransactionContext";
+import "../i18n";
 
 storiesOf("Transaction|TransationList", module)
   .add("default", () => {
@@ -13,15 +14,15 @@ storiesOf("Transaction|TransationList", module)
   })
   .add("with transactions", () => {
     const initialState = [
-      { createdAt: new Date(), "7%": -20.01, "Pfand": 0.3, total: 230.29 },
-      { createdAt: new Date(), "SEPA": 200, total: 250 },
-      { createdAt: new Date(), "BAR": 50, total: 50 },
-    ]
+      { createdAt: new Date(), "7%": -20.01, Pfand: 0.3, total: 230.29 },
+      { createdAt: new Date(), SEPA: 200, total: 250 },
+      { createdAt: new Date(), BAR: 50, total: 50 }
+    ];
     return (
-          <TransactionProvider initialState={initialState}>
-            <div className="container mx-auto p-2 flex flex-row flex-wrap">
-              <TransactionList />
-            </div>
-          </TransactionProvider>
+      <TransactionProvider initialState={initialState}>
+        <div className="container mx-auto p-2 flex flex-row flex-wrap">
+          <TransactionList />
+        </div>
+      </TransactionProvider>
     );
-  })
+  });
