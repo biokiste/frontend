2e318cefad19fc8cf10a95cef6d5b5274ee897b2
Peter Holló <lordpejdar@gmail.com>
2020-01-29 11:25:38 +0100

    Set up i18n

--- src/components/transaction/TransactionList.js
+++ src/components/transaction/TransactionList.js
@@ -2,10 +2,14 @@ import React from "react";
 import { PurchaseCategories } from "../../consts";
 import { useTransactions } from "./TransactionContext";
 import { toCurrency } from "../../utils/numbers";
+import { useTranslation } from "react-i18next";
 
 function TransactionList(props) {
-  const categories = Object.keys(PurchaseCategories).sort((a, b) => a.localeCompare(b));
+  const categories = Object.keys(PurchaseCategories).sort((a, b) =>
+    a.localeCompare(b)
+  );
   const keys = ["Date", ...categories, "Balance"];
+  const { t } = useTranslation();
   const { transactions } = useTransactions();
   return (
     <div className="w-full p-2">
@@ -14,7 +18,17 @@ function TransactionList(props) {
           <tr>
             {keys.map(key => {
               const category = PurchaseCategories[key];
-              return <th className={`p-4 py-2 ${category !== undefined ? "invisible md:visible" : `w-1/2 md:w-1/${categories.length + 2}`}`.trimRight()}>{category || key}</th>;
+              return (
+                <th
+                  className={`p-4 py-2 ${
+                    category !== undefined
+                      ? "invisible md:visible"
+                      : `w-1/2 md:w-1/${categories.length + 2}`
+                  }`.trimRight()}
+                >
+                  {t(category || key)}
+                </th>
+              );
             })}
           </tr>
         </thead>
@@ -23,14 +37,28 @@ function TransactionList(props) {
             const { createdAt, total } = transaction;
             return (
               <tr className={idx % 2 === 0 ? "bg-gray-100" : ""}>
-                <td className="border px-4 py-2 text-center">{createdAt.toLocaleString("de-DE", { dateStyle: "medium", timeStyle: "short" })}</td>
+                <td className="border px-4 py-2 text-center">
+                  {createdAt.toLocaleString("de-DE", {
+                    dateStyle: "medium",
+                    timeStyle: "short"
+                  })}
+                </td>
                 {categories.map(category => {
                   const value = transaction[PurchaseCategories[category]];
-                  return <td className={`border px-4 py-2 text-center invisible md:visible md:w-1/${categories.length + 2}`}>{value !== undefined ? toCurrency(value) : ""}</td>
+                  return (
+                    <td
+                      className={`border px-4 py-2 text-center invisible md:visible md:w-1/${categories.length +
+                        2}`}
+                    >
+                      {value !== undefined ? toCurrency(value) : ""}
+                    </td>
+                  );
                 })}
-                <td className="border px-4 py-2 text-center">{toCurrency(total)}</td>
+                <td className="border px-4 py-2 text-center">
+                  {toCurrency(total)}
+                </td>
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
