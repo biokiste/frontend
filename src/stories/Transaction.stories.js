a4f36871c99c41f5f23ea2108ca1009370074ede
Peter Holló <lordpejdar@gmail.com>
2020-02-12 22:38:58 +0100

    Add transactions structured like api data

--- src/stories/Transaction.stories.js
+++ src/stories/Transaction.stories.js
@@ -2,61 +2,100 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import TransactionList from "../components/transaction/TransactionList";
 import "../i18n";
-import { PurchaseCategories } from "../consts";
 
-storiesOf("Transaction|TransactionList", module)
-  .add("default", () => {
-    const categories = [
-      { id: 0, name: "first", type: "Number" },
-      { id: 1, name: "second", type: "Number" },
-      { id: 2, name: "third", type: "Date" }
-    ];
-    return <TransactionList categories={categories} />;
-  })
-  .add("with transactions", () => {
-    const categories = [
-      {
-        name: "createdAt",
-        type: "Date"
-      },
-      {
-        name: PurchaseCategories.CashPayment,
-        type: "Number"
-      },
-      {
-        name: PurchaseCategories.Deposit,
-        type: "Number"
-      },
-      {
-        name: PurchaseCategories.GiroTransfer,
-        type: "Number"
-      },
-      {
-        name: PurchaseCategories.ReducedVAT,
-        type: "Number"
-      },
-      {
-        name: PurchaseCategories.VAT,
-        type: "Number"
-      },
-      {
-        name: "total",
-        type: "Number"
-      }
-    ];
-    const transactions = [
-      {
-        createdAt: new Date("2019-09-30"),
-        reducedVat: -20.01,
-        deposit: 0.3,
-        total: 230.29
-      },
-      { createdAt: new Date("2019-05-17"), cash: 50, total: 50 },
-      { createdAt: new Date("2019-09-05"), giro: 200, total: 250 }
-    ];
+const categories = [
+  {
+    id: 1,
+    type: "payment",
+  },
+  {
+    id: 2,
+    type: "deposit",
+  },
+  {
+    id: 3,
+    type: "correction",
+  },
+  {
+    id: 4,
+    type: "percent19",
+  },
+  {
+    id: 5,
+    type: "percent7",
+  },
+  {
+    id: 6,
+    type: "paymentSepa",
+  },
+];
+
+const data = {
+  balance: 17.99,
+  transactions: [
+    {
+      id: 5,
+      amount: -11.64,
+      created_at: "2019-06-05 15:50:39",
+      status: 1,
+      reason: "",
+      category_id: 4,
+      type: "percent19",
+      validated_by: 0,
+    },
+    {
+      id: 4,
+      amount: -40.98,
+      created_at: "2019-06-05 15:50:39",
+      status: 1,
+      reason: "",
+      category_id: 5,
+      type: "percent7",
+      validated_by: 0,
+    },
+    {
+      id: 3,
+      amount: -19.93,
+      created_at: "2018-10-17 16:36:03",
+      status: 1,
+      reason: "",
+      category_id: 5,
+      type: "percent7",
+      validated_by: 0,
+    },
+    {
+      id: 2,
+      amount: 100,
+      created_at: "2018-10-17 16:34:59",
+      status: 1,
+      reason: "",
+      category_id: 6,
+      type: "paymentSepa",
+      validated_by: 0,
+    },
+    {
+      id: 1,
+      amount: -9.46,
+      created_at: "2018-05-30 09:56:14",
+      status: 1,
+      reason: "",
+      category_id: 4,
+      type: "percent19",
+      validated_by: 0,
+    },
+  ],
+};
+
+storiesOf("Transaction|TransactionList", module).add(
+  "with transactions",
+  () => {
     return (
       <div className="container mx-auto p-2 flex flex-row flex-wrap">
-        <TransactionList transactions={transactions} categories={categories} />
+        <TransactionList
+          transactions={data.transactions}
+          categories={categories}
+        />
       </div>
     );
-  });
+  }
+);
