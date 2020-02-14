2fe2708ce320dbfa08b75de0cd7ebeaf83e8db57
Peter Holló <lordpejdar@gmail.com>
2020-02-14 15:31:08 +0000

    Use real categories for translation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
-const { colors } = require('tailwindcss/defaultTheme');
+const { colors } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
     extend: {
       screens: {
-        landscape: { raw: "(orientation: landscape)" }
+        landscape: { raw: "(orientation: landscape)" },
       },
       width: {
         "1/7": "14.2857143%",
+        "1/8": "0.125%",
       },
       colors: {
         error: colors.red,
         warning: colors.orange,
         info: colors.green,
-      }
-    }
+      },
+    },
   },
   variants: {
-    backgroundColor: ["responsive", "hover", "focus", "active"]
+    backgroundColor: ["responsive", "hover", "focus", "active"],
   },
-  plugins: []
+  plugins: [],
 };
