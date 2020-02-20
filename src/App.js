09e06b6f0b585fcdb5b81dc8c2cbbc81f6f70545
Peter Holló <lordpejdar@gmail.com>
2020-02-20 10:19:34 +0000

    WIP: Implement Auth0 based on SPA example
    https://auth0.com/docs/quickstart/spa/react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import React from "react";
-import AppContainer from "./sites/Container";
+import React, { useEffect } from "react";
+import { Router, Route } from "react-router-dom";
+import { useAuth0 } from "./context/AuthContext";
+import { history } from "./utils/router";
+
+function Test(props) {
+  const { path } = props;
+  const {
+    user,
+    loading,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  useEffect(() => {
+    if (loading || isAuthenticated) {
+      return;
+    }
+    const fn = async () => {
+      await loginWithRedirect({
+        appState: { targetUrl: path },
+      });
+    };
+    fn();
+  }, [loading, isAuthenticated, loginWithRedirect, path]);
+
+  console.log(loading, user);
+  console.log("isAuthenticated", isAuthenticated);
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+    </div>
+  );
+}
 
 function App() {
-  return <AppContainer />;
+  const {
+    user,
+    loading,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  useEffect(() => {
+    if (loading || isAuthenticated) {
+      return;
+    }
+    const fn = async () => {
+      await loginWithRedirect({
+        appState: { targetUrl:  },
+      });
+    };
+    fn();
+  }, [loading, isAuthenticated, loginWithRedirect, path]);
+  return (
+    <Router history={history}>
+      <Route path="/" exact />
+      <Route path="/test">
+        <Test path="/test" />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,28 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 import "./i18n";
+import { history } from "./utils/router";
+import { Auth0Provider } from "./context/AuthContext";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    client_id={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
