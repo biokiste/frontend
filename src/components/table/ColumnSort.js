93e1820b9a53e3f0d2cc45eaeb146f8f69d429ec
Peter Holló <lordpejdar@gmail.com>
2020-03-02 14:56:08 +0100

    Disable interaction with table category

--- src/components/table/ColumnSort.js
+++ src/components/table/ColumnSort.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { ChevronDown, ChevronUp } from "react-feather";
 
 function ColumnSort(props) {
-  const { sortKey, direction, value, onActive, onDirection } = props;
+  const { sortKey, direction, value, onActive, onDirection, disabled } = props;
 
   const title = props.title || value;
 
@@ -18,15 +18,21 @@ function ColumnSort(props) {
   return sortKey === value ? (
     <>
       <button
-        className="focus:outline-none w-full flex flex-row justify-center font-bold"
+        className={`focus:outline-none w-full flex flex-row justify-center font-bold ${disabled &&
+          "cursor-default"}`.trimRight()}
         onClick={handleClick}
+        disabled
       >
         <div className="mr-2 truncate">{title}</div>
         {direction > 0 ? <ChevronDown size="24" /> : <ChevronUp size="24" />}
       </button>
     </>
   ) : (
-    <button className="focus:outline-none w-full font-bold" onClick={handleClick}>
+    <button
+      className={`focus:outline-none w-full font-bold ${disabled &&
+        "cursor-default"}`.trimRight()}
+      onClick={handleClick}
+    >
       {title}
     </button>
   );
